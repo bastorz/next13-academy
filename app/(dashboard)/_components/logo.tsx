84f7bb62e294c9f5adc7c8633ea2b4eff3da2256
unknown <damianalcalayy@gmail.com>
2023-10-09 16:10:38 +0100

    first commit

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
+      <Link href="/" className="flex space-x-4 hover:cursor-pointer">
+        <Image
+          height={60}
+          width={60}
+          alt="logo"
+          src="/descarga.png"
+        />
+        <h1 className="font-semibold text-lg">The Rose Academy</h1>
+      </Link>
   )
 }
